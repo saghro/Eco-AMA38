76a7ae5468d4c9f575f8dd7c8a70bd63ca379938
HossameTor <hossametorchi07@gmail.com>
2023-04-13 15:40:49 +0000

    second commit

--- car-rental/src/router/Routers.js
+++ car-rental/src/router/Routers.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import {Router,Route ,Navigate} from 'react-router-dom'
-import Home from '../pages/Home'
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Carlist from '../pages/Carlist';
+import CarDetails from '../pages/CarDetail';
+import Blog from '../pages/Blog';
+import BlogDetails from '../pages/BlogDetails';
+import NotFound from '../pages/NotFound';
+
 const Routers = () => {
   return (
-    <div>Routers</div>
+    <Routes>
+      <Route path='/' element={< Navigate to="/home"/>}/>
+      <Route path='/home' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/cars' element={<Cars/>}/> 
+      <Route path='/cars/:slug' element={<CarsDelails/>}/> 
+      <Route path='/blogs' element={<Blog/>}/>
+      <Route path='/blogs/:slug' element={<BloDelails/>}/> 
+      <Route path='*' element={<NotFound/>}/>
+
+    </Routes>
   )
 }
 
