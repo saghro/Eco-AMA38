69421f6fc063d3693a1783475f7f95f4b8b73195
HossameTor <hossametorchi07@gmail.com>
2023-04-13 15:20:00 +0000

    first commit

---
+++ car-rental/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+   <div>yuba</div>
+  );
+}
+
+export default App;
---
+++ car-rental/src/router/Routers.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Router,Route ,Navigate} from 'react-router-dom'
+import Home from '../pages/Home'
+const Routers = () => {
+  return (
+    <div>Routers</div>
+  )
+}
+
+export default Routers
